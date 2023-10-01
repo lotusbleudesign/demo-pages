c3d7f11f25cf7ba27873c43ccb76a628b75517b2
lotusbleudesign <lotus.b78@gmail.com>
2023-10-01 12:19:28 +0200

    Page index

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
-          Reload
+          How to deploy a React App with git Actions and Github Pages
         </p>
-
         <a
           className="App-link"
           href="https://reactjs.org"
